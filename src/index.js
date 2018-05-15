52a8c021a399fba073d5a2ee51ffe8cc4dd1f8a4
Kristian Kanya <kriskanya@gmail.com>
2018-05-15 19:01:12 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,2 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const ROTTEN_MEAT_PRODUCTS = ["Old Fish", "Sweeney Todd", "Tomatoes?"]
+
+const products = [<li>{ROTTEN_MEAT_PRODUCTS[0]}</li>, <li>ROTTEN_MEAT_PRODUCTS[1]</li>, <li>ROTTEN_MEAT_PRODUCTS[2]</li>]
+render() {
+  return (
+    <div>
+      { products }
+    </div>
+  )
+}
+
+export class OlderCoaster extends React.Component {
+  render() {
+    return (
+      <div class="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export class InFrontOfYou extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{{"You shouldn't look too far."}}</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
+}
+
+export class ButcherShop extends React.Component {
+  render() {
+    return (
+      <div class="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          <li>Tenderloin</li>
+          <li>Short ribs</li>
+          <li>Beef shin</li>
+          <li>Ribeye</li>
+        </ul>
+      </div>
+    )
+  }
+}
